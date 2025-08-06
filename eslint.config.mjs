a204beeaa6b8bf6115346c0496ba6d3d77514637
Abdullah Sajid <abdullahsajid1029@gmail.com>
2025-08-06 10:02:24 +0530

    Update ESLint config to disable unescaped entities rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// Merge Next.js defaults (like core-web-vitals) with custom rules
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals"], // more strict than "next"
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+      "@next/next/no-img-element": "off", // optional: suppress <img /> warning too
+    },
+  }),
+];
 
 export default eslintConfig;
