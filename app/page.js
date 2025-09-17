76920a2d4b8b8e77e306bf00a399cf51a5f52f31
Abdullah Sajid <abdullahsajid1029@gmail.com>
2025-09-17 23:00:20 +0530

    Changed project name colour

--- app/page.js
+++ app/page.js
@@ -7,20 +7,22 @@ import StartButton from "@/components/StartButton";
 export default function Home() {
 
   return (
-    <main className={styles.homepage}>
+    <>
       <AnimatedBackground />
-      <div className={styles.section1}>
-        <div className={styles.logoContainer}>
-          <div className={styles.logo}>
-            <Image fill src="/logo.png" alt="Logo" className={styles.logoImg}/>
+      <main className={styles.homepage}>
+        <div className={styles.section1}>
+          <div className={styles.logoContainer}>
+            <div className={styles.logo}>
+              <Image fill src="/logo.png" alt="Logo" className={styles.logoImg} />
+            </div>
+            <h1 className={styles.section1h1}>Portexa</h1>
           </div>
-          <h1 className={styles.section1h1}>Portexa</h1>
+          <p className={styles.section1p}>Design your portfolio with just a few clicks</p>
+          <Link href="/dashboard">
+            <StartButton />
+          </Link>
         </div>
-        <p className={styles.section1p}>Design your portfolio with just a few clicks</p>
-        <Link href="/dashboard">
-          <StartButton />
-        </Link>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
